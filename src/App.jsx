3882b5819cfc9773dfdc21818f49a0c38e295758
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-05 19:41:18 +0400

    add my first HW

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import './App.css'
 
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <h1>test</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={handleClick}>plus</button>
     </div>
   )
 }
