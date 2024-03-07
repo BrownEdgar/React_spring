4d21e8d0bd024b8e972a50c5f4584a6b4cce8810
Гор Согомонян <gorsogomonan@Gors-MacBook-Pro.local>
2024-03-07 14:06:24 +0400

    first hw with git

---
+++ src/components/reactImages/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+export default function App() {
+    return (
+        <div className='page'>
+            <h1>About me</h1>
+            <div className='content'>
+                <img src="./images/photo.jpg" alt="" />
+                <div className='info'>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident animi quaerat labore veniam consequatur, harum deleniti doloremque, atque, ullam aperiam illum perspiciatis dolorem sed?</p>
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel doloremque corrupti nostrum? Odio sunt reprehenderit suscipit corporis doloremque dolorem, quam voluptas possimus laboriosam eaque nisi dignissimos provident omnis inventore totam eos nobis ad quod cupiditate magni ducimus necessitatibus adipisci ratione dicta. Accusamus reiciendis voluptatem non tempora facilis distinctio odit fugiat provident accusantium consequuntur ut optio itaque officia modi delectus, corrupti unde deserunt suscipit sequi.
+                    </p>
+                    <ul>
+                        <li>Hunting</li>
+                        <li>Surviving</li>
+                        <li>Graplling</li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
