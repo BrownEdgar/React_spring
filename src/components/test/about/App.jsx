38b40c1c3733f31a48f16cb927cd5fc11727e543
Melkonyan-Anahit <melkonyan.an.2004@gmail.com>
2024-03-07 16:46:18 +0400

    homework4

---
+++ src/components/test/about/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+
+    <div>
+      <h1>About nature</h1>
+      <div className='contain'>
+        <img src='./public/imageReact/photo.tree.jpg' />
+        <div className='text'>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa fugiat eos minima repellendus impedit natus laborum ipsa alias ad mollitia ducimus, eligendi deleniti quis voluptas iusto nihil inventore ullam atque!</p>
+
+          <p className='p2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni unde ipsa rerum, tempora quidem iusto nam saepe iste numquam eaque eos aliquid quibusdam a fugit aut iure aliquam quia minus, debitis quis tenetur necessitatibus sapiente animi nemo. Deserunt accusamus perspiciatis reprehenderit praesentium atque eius obcaecati mollitia quod? Numquam beatae, pariatur quibusdam, modi architecto deleniti recusandae fuga sapiente accusamus repudiandae ex!</p>
+
+          <ul>
+            <li>Lorem ipsum dolor sit.</li>
+            <li>Lorem ipsum dolor sit.</li>
+            <li>Lorem ipsum dolor sit.</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
