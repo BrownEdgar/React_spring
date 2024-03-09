5fd37cc118a09de10a2b2d0a341dd2a4e0e86c79
Art-Davtyan <artur1998davtyan@gmail.com>
2024-03-09 14:26:27 +0300

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
     setCount(count + 1);
-  }
+  };
 
   return (
     <div>
       <h1>Count: {count}</h1>
+      <h2>Hello world</h2>
       <button onClick={handleClick}>plus</button>
     </div>
-  )
+  );
 }
