559b0ad609fd1f9cb39de7e1b1ed06c757f593a5
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-03-11 21:22:56 +0400

    Homework6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import App from "./"
-import './App.css'
+import React from "react";
+import ToDo from "./components/HW6/ToDo";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <ToDo />
+    </div>
+  );
 }
--- src/components/HW5/Catalog.jsx
+++ src/components/HW5/Catalog.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import "./Catalog.scss"
+import React, { useState } from "react";
+import "./Catalog.scss";
 
 export default function Catalog() {
-    const [items, setItems] = useState([
-        {
-            id:1,
-            img:"./images/interier1.jpg",
-            title:"Simplicity",
-            info:"Explore Now"
-        },
-        {
-            id:2,
-            img:"./images/interier2.jpg",
-            title:"Yous Space",
-            info:"Shop Accessories"
-        },
-        {
-            id:3,
-            img:"./images/interier3.jpg",
-            title:"Loft Chair",
-            info:"Shop Now"
-        },
-    ])
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      img: "./images/interier1.jpg",
+      title: "Simplicity",
+      info: "Explore Now",
+    },
+    {
+      id: 2,
+      img: "./images/interier2.jpg",
+      title: "Yous Space",
+      info: "Shop Accessories",
+    },
+    {
+      id: 3,
+      img: "./images/interier3.jpg",
+      title: "Loft Chair",
+      info: "Shop Now",
+    },
+  ]);
   return (
-    <div className='Catalog'>
-      {items.map(elem=>{
+    <div className="Catalog">
+      {items.map((elem) => {
         return (
-            <div className='Catalog__item' key={elem.id}>
-                <img src={elem.img} alt={elem.title} />
-                <div className="Catalog__info">
-                <h2 className='Catalog__title'>{elem.title}</h2>
-                <p className='Catalog__text'>{elem.info}</p>
-                </div>
+          <div className="Catalog__item" key={elem.id}>
+            <img src={elem.img} alt={elem.title} />
+            <div className="Catalog__info">
+              <h2 className="Catalog__title">{elem.title}</h2>
+              <p className="Catalog__text">{elem.info}</p>
             </div>
-        )
+          </div>
+        );
       })}
     </div>
-  )
+  );
 }
