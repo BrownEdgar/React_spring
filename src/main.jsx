e55367bc89b98b4b8ce19dca5a2753566a7ac35f
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-12 20:31:33 +0400

    axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/useEffect/App'
+import App from './components/axios/App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
