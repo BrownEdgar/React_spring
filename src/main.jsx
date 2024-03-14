5de7d85367b9a0052227da9eaf30520eadb7a331
pash-vardanyan <vardanyanpash23@gmail.com>
2024-03-14 20:09:11 +0400

    lesson8

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import App from './components/App'
-=======
-import App from './components/axios/App'
->>>>>>> e55367bc89b98b4b8ce19dca5a2753566a7ac35f
+import App from './examples/1/2/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
