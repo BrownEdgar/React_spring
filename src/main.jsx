b26d9ea8e680f0e41835eb8fddfeb6f5bcb54dea
TigranHarutyunyan <tik2006yan@gmail.com>
2024-03-16 12:08:06 +0400

    HOMEWORK8

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import App from './components/Citati/Citati'
+import App from './components/HW8/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
