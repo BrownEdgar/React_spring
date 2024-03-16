ca41258b7ec382dfef65b83ca9d7278cd56c55fc
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-16 20:22:09 +0400

    Modal example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
-import About from './components/About/About'
+import React, { useState } from 'react'
+import Modal from './components/Modal/Modal';
 import './App.css'
 
 export default function App() {
+  const [cities, setCities] = useState(['Tokio', "Amsterdam", "Yerevan", "London", "Berlin"]);
+  const [curerntIndex, setCurerntIndex] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => setIsOpen(!isOpen);
+  const deletCityByIndex = () => {
+    setCities(cities.toSpliced(curerntIndex, 1));
+    setCurerntIndex(null)
+  }
+
   return (
-    <div>
-      <About />
+    <div className='App'>
+      {isOpen ? (
+        <Modal toggleModal={toggleModal} theme="dark">
+
+          <h1>Are you Sure?</h1>
+          <div className="Modal__buttons">
+            <button onClick={toggleModal}>cancel</button>
+            <button onClick={() => {
+              deletCityByIndex()
+              toggleModal()
+            }}>delete</button>
+          </div>
+
+        </Modal>
+      ) : null}
+
+      <div className="App__list">
+        {
+          cities.map((elem, index) => {
+            return (
+              <div key={elem}>
+                <h2>{elem}</h2>
+                <button onClick={() => {
+                  toggleModal()
+                  setCurerntIndex(index)
+                }}>Delete</button>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
