b16bd32a9183143716837cba889953f751a3902e
TigranHarutyunyan <tik2006yan@gmail.com>
2024-03-19 18:24:15 +0400

    hw9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
-import Citati from './components/Citati/Citati'
+import axios from "axios";
+import ToDoDel from './components/ToDoDel/ToDoDel';
 
 
 export default function App() {
-  return (
-    <div>
-      <Citati/>
+  const [toDo, setToDo] = useState([])
+  const [welcomeWindowOpen, setWelcomeWindowOpen] = useState(true)
+  const [open, setOpen] = useState(false)
+  const [index, setIndex] = useState(null)
+
+  const delbyIndex = () =>{
+    setToDo(toDo.toSpliced(index, 1))
+    setIndex(null)
+  }
+
+  const toggleToDo = () => setOpen(!open)
+  const welcomeWindowClose = () => setWelcomeWindowOpen(!welcomeWindowOpen)
+
+  useEffect(() => {
+    axios("https://dummyjson.com/todos").then((res) => {
+      setToDo(res.data.todos);
+    });
+  }, []);
+
+  return(
+    <div className="App">
+
+      {welcomeWindowOpen? (
+        <ToDoDel welcomeWindowClose={welcomeWindowClose}>
+          <h2>Enjoy!</h2>
+          <button onClick={welcomeWindowClose} className='welcome-btn'>X</button>
+        </ToDoDel>
+      ):null}
+      
+      {open? (
+        <ToDoDel toggleToDo={toggleToDo}>
+          <div className="todo-btn">
+            <button onClick={toggleToDo} className='todo-btn-close'>Close</button>
+            <button onClick={() => {delbyIndex() 
+              toggleToDo()}} className='to-do-btn-delete'>Delete</button>
+          </div>
+        </ToDoDel>
+      ) : null}
+
+      <div className="list">
+        {toDo.map((elem,index) => {
+          return (
+            <div key={elem.id} className='a'>
+              <h2>{elem.todo}</h2>
+              <button onClick={() => {toggleToDo()
+              setIndex(index)}}>X</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import App from './components/HW8/App'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
