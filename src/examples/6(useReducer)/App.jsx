093107c14ba1a60d7f6aec1874d09a0e7d1c4d55
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-26 20:24:53 +0400

    useReducer example

---
+++ src/examples/6(useReducer)/App.jsx
@@ -0,0 +1,44 @@
+import React, { useReducer } from 'react'
+import './App.scss'
+import reducer, { initialState } from './reducer';
+import ACTIONS from './actionsTypes';
+import axios from 'axios';
+
+
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleClick = () => {
+    dispatch({ type: ACTIONS.ADD_ITEM, })
+  }
+
+  const addPost = () => {
+    axios('https://jsonplaceholder.typicode.com/posts', {
+      params: {
+        _limit: 4
+      }
+    }).then(res => {
+      dispatch({
+        type: ACTIONS.ADD_POSTS, payload: {
+          posts: res.data
+        }
+      })
+
+    })
+  }
+
+  return (
+    <div className='App'>
+      <h1>state: {JSON.stringify(state, null, 1)}</h1>
+      <button onClick={handleClick}>add number</button>
+      <button onClick={() => dispatch({ type: ACTIONS.ARRAY_SORT, payload: 'DESC' })}>sort</button>
+      <button onClick={() => dispatch({ type: ACTIONS.FILL_ARRAY, payload: 12 })}>fill array</button>
+      <button onClick={addPost}>add Posts</button>
+    </div>
+  )
+}
+
+
+
+
---
+++ src/examples/6(useReducer)/actionsTypes.js
@@ -0,0 +1,15 @@
+const ACTIONS = {
+  PLUS: "PLUS",
+  MINUS: "MINUS",
+  RANDOM: "RANDOM",
+  ADD_ITEM: 'ADD_ITEM',
+  ARRAY_SORT: 'ARRAY_SORT',
+  FILL_ARRAY: 'FILL_ARRAY',
+  ADD_POSTS: 'ADD_POSTS'
+}
+
+export default ACTIONS
+
+// export const PLUS = 'PLUS';
+// export const MINUS = 'MINUS';
+// export const RANDOM = 'RANDOM';
