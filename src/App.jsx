2aa7a0079c7bd291fb1570c9cd4a0dab39f599f1
TigranHarutyunyan <tik2006yan@gmail.com>
2024-03-26 16:18:34 +0400

    hw12

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.scss'
-import classNames from 'classnames'
-import Pagination from './components/Pagination/Pagination'
+import React from 'react'
 
 export default function App() {
-  const [data, setData] = useState([])
-  const [page, setPage] = useState(1)
-  const [perPage, setPerPage] = useState(1)
-
-  useEffect(() => {
-    axios('https://jsonplaceholder.typicode.com/comments', 
-    {
-      params:{
-        limit:34
-      }
-    }).then(res => setData(res.data))
-  }, [])
-
-  const changePage = (n) => setPage(n)
-  const slice = data.slice((page * perPage) - perPage, (page * perPage))
-
-  return(
-    <div className="App">
-      <div className="App__comments">
-        {
-          slice.map(elem => {
-              return (
-                <div key={elem.id}>
-                  <span className='App__span1'>{elem.id}</span>
-                  <p>{elem.body}</p>
-                </div>
-              )
-            }
-          )
-        }
-      </div>
-      <Pagination total={34} perPage={perPage} page={page} changePage={changePage}/>
-    </div>
+  return (
+    <div>App</div>
   )
 }
-
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import App from './App'
+import Blog from './components/Blog/Blog'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
 )
