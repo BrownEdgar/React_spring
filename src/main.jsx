f9bc4ace4a7c65aea8f33ef799429655fb425199
ShantArabaghian <shantarabaghian@outlook.com>
2024-03-28 18:26:25 +0400

    reducerHW

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/paginationhw/App'
+import App from './components/useReducer/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
