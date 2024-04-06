73679737bce76ad34d8623e5b4459ee8b6845b1d
Art-Davtyan <artur1998davtyan@gmail.com>
2024-04-06 13:46:58 +0300

    HW

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./HomeWorks/HomeWorkPostForm/App";
+import App from "./HomeWorks/HomeWorkForm/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
