1a3584615b7f5e2f5c1352e7c511561c794bcfdf
gor-react <drouzzer9@gmail.com>
2024-04-09 10:39:39 +0400

    Form3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './components/HW_form/App'
+import App from './components/HW_form2/App'
 
 import "./index.css";
 
