3b7ef8e42120a22fce3bbe721be77f6d974ae68a
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-04-11 18:56:40 +0400

    HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ROUTES from "./router";
 import Blog from "./components/Pages/Blogs/Blog";
-import Navigate from "./components/Navigate/Navigate"
+import Navigate from "./components/Navigate/Navigate";
 import SIngleBlog from "./components/Pages/SIngleBlog/SIngleBlog";
 
 export default function App() {
   return (
     <>
+      <Navigate />
       <Routes>
-        <Route path="/" element={<Navigate/>}></Route>
-        <Route path={ROUTES.BLOG} element={<Blog/>}></Route>
-        <Route path={ROUTES.SINGLEBLOG} element={<SIngleBlog/>}></Route>
+        <Route path={ROUTES.BLOG} element={<Blog />} />
+        <Route path={ROUTES.SINGLEBLOG} element={<SIngleBlog />} />
       </Routes>
     </>
   );
