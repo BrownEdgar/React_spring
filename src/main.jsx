893a145b6c4c1452effc2b56bae0be74f19d1d5b
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-04-15 18:07:50 +0400

    HWSlide

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/HW17/App"
+import App from "./components/HWSLider/App"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
