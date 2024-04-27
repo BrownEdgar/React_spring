d9cf12bd6346434902ac0cde19449409ecfb8294
Artur-Manukyabn <162338995+Artur-Manukyabn@users.noreply.github.com>
2024-04-27 18:41:23 +0400

    Hw

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: "usrs",
+  name: "users",
   initialState: [],
   reducers: {
     setUSers(state, action) {
