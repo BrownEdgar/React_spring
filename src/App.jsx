e6083a0a8fecc52b52db076180698ff819d4db9b
pash-vardanyan <vardanyanpash23@gmail.com>
2024-05-14 20:50:05 +0400

    react-and

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import About from './components/About/About'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App() {
+    const [data, setData] = useState([]);
+  console.log(data);
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios(`https://jsonplaceholder.typicode.com/posts`);
+        setData(result.data);
+        
+      };
+      fetchData();
+    }, []);
+    var settings = {
+      dots: false,
+      infinite: true,
+      prev:true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    };
   return (
     <div>
-      <About />
+      <h1>Data from External Source:</h1>
+      <ul>
+      </ul>
+      <Slider {...settings}>
+    
+        {data.map((item) => (
+          <h1 key={item.id}>{item.id} {item.title}</h1>
+          
+        ))}
+    </Slider>
     </div>
-  )
+  ); 
 }
+
+
+
