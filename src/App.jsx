543d889a2cd2789214cdb5ec17fa81d4a329c3e4
pash-vardanyan <vardanyanpash23@gmail.com>
2024-05-14 22:49:47 +0400

    react-and

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,35 @@ import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./App.scss";
 
 export default function App() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   console.log(data);
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios(`https://jsonplaceholder.typicode.com/posts`);
-        setData(result.data);
-        
-      };
-      fetchData();
-    }, []);
-    var settings = {
-      dots: false,
-      infinite: true,
-      prev:true,
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1,
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(`https://jsonplaceholder.typicode.com/posts`);
+      setData(result.data);
     };
+    fetchData();
+  }, []);
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
   return (
-    <div>
+    <div className="App">
       <h1>Data from External Source:</h1>
-      <ul>
-      </ul>
       <Slider {...settings}>
-    
         {data.map((item) => (
-          <h1 key={item.id}>{item.id} {item.title}</h1>
-          
+          <h1 key={item.id}>
+            {item.id} {item.title}
+          </h1>
         ))}
-    </Slider>
+      </Slider>
     </div>
-  ); 
+  );
 }
-
-
-
